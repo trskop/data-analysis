9ef4ad91eb9492f096d93475aa1db60a16d7ddde
Peter Trško <peter.trsko@gmail.com>
2016-01-17 20:47:02 +0100

    Fixity of on and preon is now infixl 0
    Fixity infixl 0 is the same as Data.Function.on has, so it made sense to
    use it as a first try.

--- src/DataAnalysis/Lens.hs
+++ src/DataAnalysis/Lens.hs
@@ -19,14 +19,28 @@ import Data.Monoid (First)
 import Control.Lens
 
 
--- |
+-- | Perform binary function on results of two 'view' applications. This is an
+-- equivalent of 'Data.Function.on' from "Data.Function" module; only
+-- difference is that it tages 'Getting' instead of pure function @s -> a@.
 --
 -- To reconstruct 'Data.Function.on' from "Data.Function" module, just use 'to':
 --
 -- >>> ((+) `on` at id) 1 2
 -- 3
+--
+-- Usage examples:
+--
+-- >>> ((==) `on` _1) ("hello",1) ("world",1 :: Int)
+-- False
+--
+-- >>> ((==) `on` _2) ("hello",1) ("world",1 :: Int)
+-- True
 on :: (a -> a -> t) -> Getting a s a -> s -> s -> t
 (f `on` l) s1 s2 = f (s1 ^. l) (s2 ^. l)
+infixl 0 `on`
 
+-- | Similar to 'on', but instead of '^.' operator it uses '^.', which performs
+-- equivalent of \"safe head\".
 preon :: (Maybe a -> Maybe a -> t) -> Getting (First a) s a -> s -> s -> t
 (f `preon` l) s1 s2 = f (s1 ^? l) (s2 ^? l)
+infixl 0 `preon`
