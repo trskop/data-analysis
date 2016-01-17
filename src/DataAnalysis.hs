46be5d1dbd786cc033c349eb8907300c4082c18a
Peter Trško <peter.trsko@gmail.com>
2016-01-17 21:34:19 +0100

    Bunch of useful packages and re-exports

--- src/DataAnalysis.hs
+++ src/DataAnalysis.hs
@@ -16,7 +16,9 @@ module DataAnalysis
 
     , module Control.Applicative
     , module Control.Monad
+    , module Data.Bifunctor
     , module Data.Bool
+    , module Data.Char
     , module Data.Either
     , module Data.Eq
     , module Data.Foldable
@@ -26,10 +28,21 @@ module DataAnalysis
     , module Data.Maybe
     , module Data.Monoid
     , module Data.Ord
+    , module Data.Ratio
 --  , module Data.Semigroup
+    , module Data.String
     , module Data.Traversable
+    , module Data.Tuple
     , module Data.Word
 
+    , module Data.HashMap.Lazy
+    , module Data.HashSet
+    , module Data.IntMap
+    , module Data.IntSet
+    , module Data.Map
+    , module Data.Scientific
+    , module Data.Sequence
+    , module Data.Set
     , module Data.Vector
 
     , module Control.Lens
@@ -40,15 +53,26 @@ module DataAnalysis
     , module DataAnalysis.Groupable.Class
     , module DataAnalysis.Lens
     , module DataAnalysis.Sortable
+    , module DataAnalysis.Type
     , module DataAnalysis.Type.Encoding
     )
   where
 
-import Prelude (Bounded(..), Num(..), Integral(..))
+import Prelude
+    ( Bounded(..)
+    , Enum(..)
+    , Integral(..)
+    , Num(..)
+    , Integer
+    , Float
+    , Double
+    )
 
 import Control.Applicative
 import Control.Monad
+import Data.Bifunctor
 import Data.Bool
+import Data.Char
 import Data.Either
 import Data.Eq
 import Data.Foldable
@@ -58,10 +82,22 @@ import Data.Int
 import Data.Maybe
 import Data.Monoid
 import Data.Ord
+import Data.Ratio
 --import Data.Semigroup
+import Data.String
 import Data.Traversable
+import Data.Tuple
 import Data.Word
 
+import Data.HashMap.Lazy (HashMap)
+    -- Data type is the same for stirct and lazy HashMap.
+import Data.HashSet (HashSet)
+import Data.IntMap (IntMap)
+import Data.IntSet (IntSet)
+import Data.Map (Map)
+import Data.Scientific
+import Data.Sequence (Seq)
+import Data.Set (Set)
 import Data.Vector
     ( Vector
     , fromList
@@ -70,10 +106,13 @@ import Data.Vector
 import Control.Lens
 --import Data.Csv hiding ((.=), index)
 import Data.Default.Class
+import Data.Default.Instances.Base ()
+import Data.Default.Instances.Containers ()
 
 import DataAnalysis.Decode
 import DataAnalysis.Encode
 import DataAnalysis.Groupable.Class
 import DataAnalysis.Lens
 import DataAnalysis.Sortable
+import DataAnalysis.Type
 import DataAnalysis.Type.Encoding
